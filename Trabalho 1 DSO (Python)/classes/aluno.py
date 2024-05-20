7ffe00fd2e33a0dcff5c3049afaf00b363e28286
Thiago Coutinho <thicoutinhosilva@gmail.com>
2024-05-20 17:53:25 -0300

    Add files via upload

---
+++ Trabalho 1 DSO (Python)/classes/aluno.py
@@ -0,0 +1,46 @@
+from classes.abstractPessoa import Pessoa
+from classes.curso import Curso
+
+
+class Aluno(Pessoa):
+    def __init__(self, nome: str, cpf: int, dia: int, mes: int, ano: int, curso: Curso, matricula: int, quantidade_gols: int):
+        super().__init__(nome=nome, cpf=cpf, dia=dia, mes=mes, ano=ano)
+        self.__curso = None
+        self.__matricula = None
+        self.__quantidade_gols = None
+
+        if isinstance(curso, Curso):
+            self.__curso = curso
+
+        if isinstance(matricula, int):
+            self.__matricula = matricula
+
+        if isinstance(quantidade_gols, int):
+            self.__quantidade_gols = quantidade_gols
+
+    @property
+    def curso(self):
+        return self.__curso
+
+    @property
+    def matricula(self):
+        return self.__matricula
+
+    @property
+    def quantidade_gols(self):
+        return self.__quantidade_gols
+
+    @curso.setter
+    def curso(self, curso):
+        if isinstance(curso, Curso):
+            self.__curso = curso
+
+    @matricula.setter
+    def matricula(self, matricula):
+        if isinstance(matricula, int):
+            self.__matricula = matricula
+
+    @quantidade_gols.setter
+    def quantidade_gols(self, quantidade_gols):
+        if isinstance(quantidade_gols, int):
+            self.__quantidade_gols = quantidade_gols
---
+++ Trabalho 1 DSO (Python)/classes/arbitro.py
@@ -0,0 +1,17 @@
+from classes.abstractPessoa import Pessoa
+
+
+class Arbitro(Pessoa):
+    def __init__(self, nome, cpf, dia, mes, ano):
+        super().__init__(nome=nome, cpf=cpf, dia=dia, mes=mes, ano=ano)
+        
+        self.__numero_de_partidas = 0
+    
+    @property
+    def numero_de_partidas(self):
+        return self.__numero_de_partidas
+
+    @numero_de_partidas.setter
+    def numero_de_partidas(self, numero_de_partidas):
+        if isinstance(numero_de_partidas, int):
+            self.__numero_de_partidas = numero_de_partidas
